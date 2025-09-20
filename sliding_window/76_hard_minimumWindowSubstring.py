adb4e29fd9e91e85d37202f3d615115e89635f2a
khanhtd <khanhtd@ti-one.de>
2025-09-20 16:57:21 +0700

    minWindow

--- sliding_window/76_hard_minimumWindowSubstring.py
+++ sliding_window/76_hard_minimumWindowSubstring.py
@@ -16,16 +16,16 @@ class Solution:
             if s[r] in count_t and count_win[s[r]] == count_t[s[r]]:
                 have += 1
             
-            if have == need:
-                while have == need:
-                    count_win[s[l]] -= 1
-                    if s[l] in count_t and count_win[s[l]] < count_t[s[l]]:
-                        have -= 1
-                    l += 1
-                if res_len > (r - (l - 1) + 1):
-                    res_len = (r - (l - 1) + 1)
-                    res_pos = [l-1, r+1]
-            r += 1
+            while have == need:
+                if res_len > (r - l + 1):
+                    res_len = (r - l + 1)
+                    res_pos = [l, r+1]
+
+                count_win[s[l]] -= 1
+                if s[l] in count_t and count_win[s[l]] < count_t[s[l]]:
+                    have -= 1
+                l += 1
+                
         l, r = res_pos
         return s[l: r]
 
@@ -33,3 +33,14 @@ sol = Solution()
 s = "OUZODYXAZV"
 t = "XYZ"
 print(sol.minWindow(s, t))
+
+# Bài toán yêu cần tìm chuỗi con ngắn nhất trong chuỗi s chứa tất cả các ký tự của chuỗi t.
+# Nhưng chuỗi con t có thế chứa ký tự trùng lặp. nên làm bài này khó hơn
+# Cách giải quyết là dùng sliding window với 2 con trỏ l, r
+# Nhưng thay vì chỉ cần tất cả các ký tự trong t xuất hiện trong cửa sổ [l, r]
+# thì cần phải đảm bảo tần suất xuất hiện của các ký tự trong cửa sổ [l, r] phải lớn hơn hoặc bằng tần suất xuất hiện của các ký tự trong t
+# Để làm được điều này thì dùng 2 dict để đếm tần suất xuất hiện của các ký tự trong t và trong cửa sổ [l, r]
+# Sau đó dùng biến have để đếm số lượng ký tự trong cửa sổ [l, r] có tần suất xuất hiện bằng tần suất xuất hiện trong t
+# Khi have == need thì có nghĩa là tất cả các ký tự trong t đều xuất hiện trong cửa sổ [l, r] với tần suất đúng
+# Lúc này thì thu nhỏ cửa sổ [l, r] bằng cách dịch con trỏ l sang phải
+# Đồng thời cập nhật kết quả nếu cửa sổ hiện tại nhỏ hơn kết quả trước đó.
